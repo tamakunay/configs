aa9c58ad5f7701762fa868b3aba46a75ec79da17
Tafadzwa Makuyana <tgmakuyana@hotmail.com>
2023-07-18 15:48:04 +0700

    remove unused and configs

--- mac/aliases.zsh
+++ mac/aliases.zsh
@@ -11,25 +11,16 @@ alias dt="datree"
 # mac OS shortcuts
 alias code="open -a 'Visual Studio Code'"
 
-
-
 # ALIAS COMMANDS
 alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons --group-directories-first -l"
 alias g="goto"
 alias grep='grep --color'
 
-alias cbp="code /home/xcad/obsidianvault/boilerplates"
-alias cpr="code /home/xcad/obsidianvault/projects"
-
-# Scripts
-alias obsidian="python3 /Users/xcad/Projects/christianlempa/scripts/obsidian"
-
 
 alias zshconf="code ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
-# alias r ="source ~/.zshrc"
 
 #code
 alias lab="cd dev/Builds"
@@ -39,8 +30,6 @@ alias taf="cd dev/taf.codes"
 alias wrk="cd dev/work"
 alias alpn="cd dev/alpn"
 alias study="cd dev/study"
-alias mwd="cd dev/work/resource_one"
-
 
 
 #git
@@ -91,13 +80,9 @@ alias yrtc="yarn test:c" #test with coverage
 alias yp="yarn build && npm publish"
 alias yu="npm unpublish "
 
-alias tmk_git="git config user.name 'Tafadzwa Makuyana' && git config user.email 'tgmakuyana@hotmail.com'"
-alias alpn_git="git config user.name 'Alpn Tech' && git config user.email 'alpntech.solutions@gmail.com'"
-
 
 alias vsc="code ."
 alias vcp="vsce package"
-alias python=/usr/bin/python3
 
 
 alias phps="php -S localhost:8000"
@@ -110,18 +95,17 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# mysql
-alias mysql="mysql -h localhost -P 3306 --protocol=tcp -u root -p"
-
-
-
 
 # brew
 alias brewl="brew services list"
+
+# mysql
+alias mysql="mysql -h localhost -P 3306 --protocol=tcp -u root -p"
 alias mysqlstart="brew services start mysql "
 alias mysqlstop="brew services stop mysql "
 
-alias mysqlstart="brew services start mongodb-community"
-alias mysqlstop="brew services stop mongodb-community"
+# mongo
+alias mongostart="brew services start mongodb-community"
+alias mongostop="brew services stop mongodb-community"
 
 
